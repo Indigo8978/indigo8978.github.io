500aea44368dc69d0336360bc31de6379b6d4e35
Indigo8978 <fuwang3344@gmail.com>
2023-02-20 16:54:58 -0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,52 @@
+let video = null;
+let canvas = null;
+let context = null;
+let scaler = 0.5;
+function main() {
+    canvas = document.getElementById("myCanvas");
+    context = canvas.getContext("2d");
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    let promise=navigator.mediaDevices.getUserMedia({video:true});
+    promise.then(function(signal) {
+        video = document.createElement("video")
+        video.srcObject = signal;
+        video.play();
+
+        video.onloadeddata = function() {
+            updateCanvas();
+        }
+    })
+}
+
+document.getElementById('fell').addEventListener('click', function(){
+    alert("Haha L I cant believe you fell for it")
+    return;
+});
+
+function harharhar() {
+    alert("you're about to die :D");
+}
+
+let counter = 59;
+function timeout() {
+    setTimeout(function () {
+    document.getElementById('time').innerHTML = counter--;
+    timeout();
+}, 1000);
+}
+
+/*
+*/
+timeout();
+setTimeout(harharhar, 61000);
+
+function updateCanvas() {
+    context.drawImage(video, 0, 0, 200, 200);
+    window.requestAnimationFrame(updateCanvas);
+}
+
+function handleResize() {
+
+}
